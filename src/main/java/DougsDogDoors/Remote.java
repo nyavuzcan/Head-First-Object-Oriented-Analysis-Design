8ebbe1099ad84a5988deecc472b672af320d9dd6
Nevzat YAVUZCAN <nevzat.yavuzcan@etiya.com>
2021-05-17 02:48:58 +0300

    Added automatically close door function

--- src/main/java/DougsDogDoors/Remote.java
+++ src/main/java/DougsDogDoors/Remote.java
@@ -1,5 +1,11 @@
 package DougsDogDoors;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+/*
+Added schedule for auto close door.
+ */
 public class Remote {
 
   private DogDoor door;
@@ -14,6 +20,20 @@ public class Remote {
       door.close();
     }else {
       door.open();
+      /*
+        made it final because we need to access cancel() in the TimerTask anonymous class.
+        Also Time quite automatically after running the TimerTask but
+        JVMs take forever before they garbage collect the timer. It can be hours. So we can use cancel() manually.
+
+       */
+      final Timer timer = new Timer();
+      timer.schedule(new TimerTask() {
+        @Override
+        public void run() {
+          door.close();
+          timer.cancel();
+        }
+      },5000);
     }
   }
 }
